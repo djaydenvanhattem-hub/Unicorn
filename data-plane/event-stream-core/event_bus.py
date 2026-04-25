af9742fd15cd1c23af5d72822e2c29db5f361a8e
djaydenvanhattem-hub <djaydenvanhattem@icloud.com>
2026-04-25 22:44:44 +0200

    Implement basic EventBus class for event handling

---
+++ data-plane/event-stream-core/event_bus.py
@@ -0,0 +1,9 @@
+class EventBus:
+    def __init__(self):
+        self.events = []
+
+    def publish(self, event):
+        self.events.append(event)
+
+    def consume(self):
+        return self.events
